43d919cc598c2e2f6c955ea20c4e849a49a7a171
Lipedev300 <luizfelipebasso22@gmail.com>
2025-08-13 10:54:02 -0300

    Incluído arquivo CSS para o estilo ficar separado da estrutura da página. Incluído no formulário o campo unidade do produto para indicar se ele é por exemplo em gramas, litros, quilos a embalagem

--- Script.js
+++ Script.js
@@ -1,26 +1,38 @@
 let item = document.getElementById("item");
 let categoria = document.getElementById("tipo");
+let quantidade = document.getElementById("quantity");
+let unidade = document.getElementById("unity");
 let listacompras = document.getElementById("listacompras")
 let btnAdproduto = document.getElementById("additem");
 
 btnAdproduto.addEventListener("click", function() {
     let textoInserido = item.value.trim();
     if (textoInserido == "") {
-        console.log("Por favor meu caro, digite algo aqui");
+        alert("Por favor meu caro, digite algo aqui");
     } else {
         let elemento = document.createElement("li");
 let nomeProduto = document.createElement("p");
 let categoriaProduto = document.createElement("span");
+let quantidadeProduto = document.createElement("p");
+let unidadeProduto = document.createElement("p");
 
         let compra = {
+            idDoProduto: Date.now(),
             nomeDoProduto: item.value,
-            categoriaDoProduto: categoria.value
+            categoriaDoProduto: categoria.value,
+            quantidadeDoProduto: quantidade.value,
+            unidadeDoProduto: unidade.value
         }
         
         nomeProduto.textContent = compra.nomeDoProduto;
         categoriaProduto.textContent = compra.categoriaDoProduto;
+        quantidadeProduto.textContent = compra.quantidadeDoProduto;
+        unidadeProduto.textContent = compra.unidadeDoProduto;
+
         elemento.appendChild(nomeProduto);
         elemento.appendChild(categoriaProduto);
+        elemento.appendChild(quantidadeProduto);
+        elemento.appendChild(unidadeProduto);
         listacompras.appendChild(elemento);
         alert("Produto adicionado com sucesso!");
     }
